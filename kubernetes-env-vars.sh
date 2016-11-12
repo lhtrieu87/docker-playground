d9fb9747ffe4d7805edc608e3fc6c03bb4b889b9
Le Hong Trieu <lehongtrieu@gmail.com>
2016-11-12 10:49:05 +0800

    Kubernetes setup scripts

---
+++ kubernetes-env-vars.sh
@@ -0,0 +1,8 @@
+export KUBERNETES_PROVIDER=aws
+export KUBE_AWS_ZONE=ap-southeast-1a
+export NUM_NODES=1
+export MASTER_SIZE=t2.micro
+export NODE_SIZE=t2.medium
+export AWS_S3_REGION=ap-southeast-1
+export AWS_S3_BUCKET=trieu-kubernetes-artifacts
+export KUBE_AWS_INSTANCE_PREFIX=k8s
